0de546fb300a6d05915f294069e6fefa7d7dfabf
Amit Sharma <amitsharma@Amits-MacBook-Air.local>
2023-02-18 21:31:46 +0530

    Layout update

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,24 +1,19 @@
 import * as React from 'react'
-import Sidebar from '../Sidebar/Sidebar'
-import SpotifyPlayer from '../SongPlayer/SpotifyPlayer'
+
 import Header from './Header'
+import Sidebar from '../Sidebar/Sidebar'
 import Player from '../SongPlayer/Player'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <>
             <div className='flex h-screen overflow-y-hidden bg-dark'>
-                {/* <Header />
-                <div className='flex h-full overflow-scroll'>
-                    <Sidebar />
-                    {children}
-                </div>
-                <SpotifyPlayer />
-                <Player /> */}
                 <Sidebar />
-                <div className='w-full'>
+                <div className='relative w-full'>
                     <Header />
-                    {children}
+                    <div className='absolute top-[66px] right-0 bottom-20 w-full'>
+                        {children}
+                    </div>
                     <Player />
                 </div>
             </div>
